fa1d2ea5529092ce134d618d5d157689fed258f3
Michael Poust <michael.poust221@gmail.com>
2022-04-13 18:54:26 -0400

    flip cards for my services

---
+++ source/PoustResume/Components/Resume/WhatIDo/FlipCardComponent.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PoustResume.Components.Resume.WhatIDo;
+
+/// <summary>
+///     Partial class providing logic for the Flip Card templated component
+/// </summary>
+public partial class FlipCardComponent
+{
+    /// <summary>
+    ///     Route to send the on-click action of the Flip Card back
+    /// </summary>
+    [Parameter]
+    public string NavigationRoute { get; set; }
+
+    /// <summary>
+    ///     Template to display on the front of the flip card
+    /// </summary>
+    [Parameter]
+    public RenderFragment FrontTemplate { get; set; }
+
+    /// <summary>
+    ///     Template to display on the back of the flip card
+    /// </summary>
+    [Parameter]
+    public RenderFragment BackTemplate { get; set; }
+
+
+    /// <summary>
+    ///     Navigate to the Route provided as component parameter
+    /// </summary>
+    /// <param name="navRoute"></param>
+    void NavigateOnClick()
+    {
+        NavManager.NavigateTo($"/{NavigationRoute}");
+    }
+
+}
