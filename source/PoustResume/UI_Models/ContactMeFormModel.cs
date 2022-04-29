7e48806760cc6375fd8f73b3411a9a3e9da59237
Michael Poust <michael.poust221@gmail.com>
2022-04-29 12:22:11 -0400

    adding contact me form placeholder; footer placeholder; and style experiments

---
+++ source/PoustResume/UI_Models/ContactMeFormModel.cs
@@ -0,0 +1,27 @@
+﻿namespace PoustResume.UI_Models;
+
+/// <summary>
+///     Model to hold the data for submitting a contact me form request
+/// </summary>
+public class ContactMeFormModel
+{
+    /// <summary>
+    ///     Name of the person submitting the contact request
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    ///     Email address of the person submitting the contact request
+    /// </summary>
+    public string Email { get; set; }
+
+    /// <summary>
+    ///     Subject of the contact request
+    /// </summary>
+    public string Subject { get; set; }
+
+    /// <summary>
+    ///     Message content for the contact request
+    /// </summary>
+    public string Message { get; set; }
+}
