f5a8517b594ba94e215e1452f586e06faa2444ad
Michael Poust <michael.poust221@gmail.com>
2022-05-01 15:14:27 -0400

    attempt to convert skills into a carousel

---
+++ source/PoustResume/UI_Models/SkillCarouselModel.cs
@@ -0,0 +1,22 @@
+﻿namespace PoustResume.UI_Models;
+
+/// <summary>
+///     Model for displaying skills in a Carousel
+/// </summary>
+public class SkillCarouselModel
+{
+    /// <summary>
+    ///     ID for the tab of the Carousel
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    ///     Title of the Skill in the Carousel
+    /// </summary>
+    public string Title { get; set; }
+
+    /// <summary>
+    ///     Level of the skill to display in the gauge
+    /// </summary>
+    public double? SkillLevel { get; set; }
+}
